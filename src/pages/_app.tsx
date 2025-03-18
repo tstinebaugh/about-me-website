d167ecd82004da8b2af85ecab5732b420e149983
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2025-03-18 06:13:37 -0700

    Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import darkTheme from "../themes/darkTheme";
 import Layout from "@/components/layout/Layout";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <SpeedInsights />
+      <Analytics />
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Layout>

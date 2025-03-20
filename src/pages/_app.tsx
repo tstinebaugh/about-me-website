2bb981eb6a33317e4901dca2e5b9b1e960b8757c
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2025-03-20 07:27:14 -0700

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,14 @@ import darkTheme from "../themes/darkTheme";
 import Layout from "@/components/layout/Layout";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <CssBaseline />
       <Layout>
         <Component {...pageProps} />

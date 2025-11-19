3107bdcf33b91347bebd6a59dc7416f6c43a310c
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2025-11-19 00:49:24 -0800

    fix(a11y): Improve color contrast for primary buttons in dark theme

--- src/themes/darkTheme.ts
+++ src/themes/darkTheme.ts
@@ -4,7 +4,7 @@ const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: "#6366f1", // Indigo color
+      main: "#5254D1", // Indigo color
       light: "#818cf8",
       dark: "#4f46e5",
     },
